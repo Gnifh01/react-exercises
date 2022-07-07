bcdbd96f380d16834ce704a386e948c05c47fc0f
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-07-07 18:19:20 +0200

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Message = () => {
-  return <p>What a beautiful day!</p>;
-}
-
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <Message />
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <HelloWorld />
-      </div>
-    );
-  }
-}
-
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
