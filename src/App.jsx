f1de09ff1560f89b847cb078c7b9c8f957a9fbce
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-07-11 15:33:56 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props = props;
+  }
   render() {
-    return <p>Welcome {this.props.name}!</p>;
+    const {name, age} = this.props;
+    return (
+      <React.StrictMode>
+        <p>Welcome {name}!</p>
+        <p>Your age is {age}</p>
+      </React.StrictMode>
+    );
   }
 }
 
 //YES, I CAN SET DEFAULT VALUE IN PROPS WITH THIS METHOD :
-
 Welcome.defaultProps = {
   name: "Gabbo",
 };
 
 export default class App extends React.Component {
   render() {
-    return <Welcome />;
+    return <Welcome age={20} />;
   }
 }
