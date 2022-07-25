edba7d8e08e221464c0ea9cb29004e50869984ee
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-07-25 16:54:12 +0200

    first commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -15,40 +15,47 @@ export default class Login extends Component {
     });
   };
 
-  componentDidUpdate() {
+  onLogin = () => {
     console.log(this.state);
-  }
+  };
 
   render() {
     return (
       <div>
-        <label>Username</label>
-        <br />
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.hendleInput}
-        />
-        <br />
-        <br />
-        <label>Password</label>
-        <br />
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.hendleInput}
-        />
-        <br />
-        <br />
-        <input
-          type="checkbox"
-          name="remember"
-          checked={this.state.remember}
-          onChange={this.hendleInput}
-        />
-        <label>Remember</label>
+        <div>
+          <label>Username</label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.hendleInput}
+          />
+          <br />
+          <br />
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.hendleInput}
+          />
+          <br />
+          <br />
+          <input
+            type="checkbox"
+            name="remember"
+            checked={this.state.remember}
+            onChange={this.hendleInput}
+          />
+          <label>Remember</label>
+        </div>
+        <div>
+          {this.state.username && this.state.password ? (
+            <button onClick={this.onLogin}>Login</button>
+          ) : null}
+        </div>
       </div>
     );
   }
